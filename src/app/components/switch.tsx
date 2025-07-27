af5dfe38eef5a980749ee3d505cf7459ead45a89
Krish Daterao <akdaterao@gmail.com>
2025-07-27 14:42:13 -0400

    tweaking some stuff

--- src/app/components/switch.tsx
+++ src/app/components/switch.tsx
@@ -8,9 +8,9 @@ function Dark_Switch(){
     return(
 
         <div className ='h-8 w-20 relative  rounded-full outline-[var(--color-navbar-t)] outline-6 '>
-            <label className ={`h-full w-full relative  rounded-full flex flex-none bg-none outline-[var(--color-navbar-t)]  ${Darkstate ? '' : ''}`}>
+            <label className ={`h-full w-full relative  rounded-full flex  bg-none outline-[var(--color-navbar-t)]   ${Darkstate ? '' : ''}`}>
                 <input type="checkbox" onClick={() => { changestate(!Darkstate); document.documentElement.classList.toggle('dark'); }} className='invisible h-full w-full'/>
-                <span className={`h-8 w-12  rounded-full outline-3 outline-[var(--color-outline)] justify-self-start bg-[var(--color-navbar)] transition duration-75 ease-in-out flex ${Darkstate ? '-translate-x-1' : '-translate-x-12'}`}></span>
+                <span className={`h-8 w-8  rounded-full outline-3 outline-[var(--color-outline)]  bg-[var(--color-navbar)] transition duration-75 ease-in-out flex flex-shrink-0 ${Darkstate ? '-translate-x-1' : '-translate-x-12'}`}></span>
             </label>
         </div>
 
